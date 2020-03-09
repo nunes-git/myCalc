73a3c4ba4a2a83017f8e2205e6b35ec034ddfc8a
nunes-git <61033507+nunes-git@users.noreply.github.com>
2020-03-09 17:08:53 -0300

    Add files via upload

--- Scripts/Controller/calcController.js
+++ Scripts/Controller/calcController.js
@@ -6,10 +6,33 @@ class calcController {
   }
 
   initialize(){
-    console.log("agora to aqui");
+    this.getButton();
 
   }
 
+  execBtn(){
+    // switch()
+  }
 
+  getButton(){
 
+    // let buttons = document.querySelectorAll("div > #buttons");
+    let buttons = document.getElementById("buttons");
+    console.log(buttons);
+
+    // buttons.forEach(btn => {
+    //   document.addEventListener("click", e=>{
+    //     console.log(e);
+    //   });
+    // });
+
+    buttons.forEach((btn, index) => {
+      
+      document.addEventListener("click", e=>{
+        let textBtn = btn.innerHTML;
+        console.log(e);
+        this.execBtn(textBtn);
+        });
+    });
+  }
 }
